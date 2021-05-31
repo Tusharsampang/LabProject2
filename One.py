359fd82b62ca2478ead811b9aa45190c8a6ca6ea
ACER <tusharsampang@gmail.com>
2021-05-31 12:09:09 +0545

    initial commit

---
+++ One.py
@@ -0,0 +1,10 @@
+'''
+1.Write a program that takes numbers and print theirs sum.Every number is given on a separate line.
+'''
+
+num1 = int(input("enter the first value: "))
+num2 = int(input("enter the second value: "))
+num3 = int(input("enter the third value: "))
+sum = num1 + num2 + num3
+print(sum)
+print(f"The sum of given three numbers is {sum}")
---
+++ OuestionTwo.py
@@ -0,0 +1,9 @@
+'''
+2.Write a program that reads the length of the base and the height of a right-angled triangle and prints the area.
+ Every number is given on a separate line.
+'''
+
+base = int(input("enter the length of the base (in cm) of a right-angled triangle:"))
+height = int(input("enter the height (in cm) of the right-angled triangle:"))
+area =(1/2)*base*height
+print(f"area of the right agnled triangle is : {area} sq.cm")
